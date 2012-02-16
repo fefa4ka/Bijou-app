032e8cdfc54cb53b20284393087f9b874205c33f
fefa4ka <fefa4ka@gmail.com>
2012-02-16 23:16:01 +0400

    Clear

--- app/models/can_helps_help_types.rb
+++
@@ -1,2 +0,0 @@
-class CanHelpsHelpTypes < ActiveRecord::Base
-end
--- app/models/can_helps_place.rb
+++
@@ -1,5 +0,0 @@
-class CanHelpsPlace < ActiveRecord::Base
-  validates_presence_of :can_help_id, :place_id
-  belongs_to :can_help
-  belongs_to :place
-end
