2d85f2bb79ad59d50817e6395a36387922121940
fefa4ka <fefa4ka@gmail.com>
2012-04-05 14:40:46 +0400

    Don't know.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 
 after 'deploy:update', 'whenever:update_crontab'
-after 'deploy:update', 'deploy:assets:precompile'
 after 'deploy:restart', 'unicorn:stop'
 after 'deploy:restart', 'thinking_sphinx:stop'
 
